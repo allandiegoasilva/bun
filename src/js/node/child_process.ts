9cc325cba01b1be8ec0deb901bb3fa82993f1351
Allan Diego <allandiego252@gmail.com>
2025-07-11 23:15:17 -0300

    feat: add default args like -1 to be ignored

--- src/js/node/child_process.ts
+++ src/js/node/child_process.ts
@@ -1319,8 +1319,8 @@ class ChildProcess extends EventEmitter {
         onDisconnect: has_ipc ? ok => this.#onDisconnect(ok) : undefined,
         serialization,
         argv0: spawnargs[0],
-        uid: options.uid || undefined,
-        gid: options.gid || undefined,
+        uid: options.uid || -1,
+        gid: options.gid || -1,
         windowsHide: !!options.windowsHide,
         windowsVerbatimArguments: !!options.windowsVerbatimArguments,
       });
